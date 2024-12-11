76a13ddef9ab0e75c2dd4e01d5b37d5e3d66e280
Pitchaya T. <33589608+ptchayap@users.noreply.github.com>
2024-12-11 18:17:29 +0700

    fix: see more position to be the same line (#767)

--- src/v4/social/internal-components/TextWithMention/TextWithMention.tsx
+++ src/v4/social/internal-components/TextWithMention/TextWithMention.tsx
@@ -1,6 +1,5 @@
-import { SerializedParagraphNode, SerializedTextNode } from 'lexical';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-import { Typography } from '~/v4/core/components';
+import { SerializedLexicalNode, SerializedParagraphNode } from 'lexical';
+import React, { useMemo, useState } from 'react';
 import { Mentioned, Mentionees } from '~/v4/helpers/utils';
 import styles from './TextWithMention.module.css';
 import {
@@ -14,7 +13,8 @@ import {
 import clsx from 'clsx';
 import { useNavigation } from '~/v4/core/providers/NavigationProvider';
 import { Button } from '~/v4/core/natives/Button/Button';
-
+import { Typography } from '~/v4/core/components';
+import Truncate from 'react-truncate-markup';
 interface TextWithMentionProps {
   pageId?: string;
   componentId?: string;
@@ -37,9 +37,6 @@ export const TextWithMention = ({
   metadata,
 }: TextWithMentionProps) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const [isClamped, setIsClamped] = useState(false);
-  const [isHidden, setIsHidden] = useState(false);
-  const fullContentRef = useRef<HTMLDivElement>(null);
 
   const { goToUserProfilePage } = useNavigation();
 
@@ -53,118 +50,76 @@ export const TextWithMention = ({
     [data, mentionees, metadata],
   );
 
-  useEffect(() => {
-    // check if should be clamped or not, then hide the full content
-    const fullContentHeight = fullContentRef.current?.clientHeight || 0;
-
-    const rootFontSize = parseInt(
-      window.getComputedStyle(document.body).getPropertyValue('font-size'),
-      10,
-    );
-
-    const clampHeight =
-      parseFloat(
-        getComputedStyle(document.documentElement).getPropertyValue('--asc-line-height-md'),
-      ) * rootFontSize;
-
-    if (fullContentHeight > clampHeight * maxLines) {
-      setIsClamped(true);
+  const convertSerializedToText = (child: SerializedLexicalNode) => {
+    if ($isSerializedMentionNode<MentionData>(child)) {
+      return (
+        <span
+          data-qa-anchor={`${pageId}/${componentId}/mention`}
+          key={child.data.userId}
+          className={clsx(styles.textWithMention__mention)}
+          onClick={() => goToUserProfilePage(child.data.userId)}
+          tabIndex={0}
+        >
+          {child.text}
+        </span>
+      );
+    }
+    if ($isSerializedAutoLinkNode(child) || $isSerializedLinkNode(child)) {
+      return (
+        <a
+          key={child.url}
+          href={child.url}
+          className={clsx(styles.textWithMention__link)}
+          data-qa-anchor={`${pageId}/${componentId}/post_link`}
+          target="_blank"
+        >
+          {$isSerializedTextNode(child.children[0]) ? child.children[0]?.text : child.url}
+        </a>
+      );
     }
 
-    setIsHidden(true);
-  }, []);
+    if ($isSerializedTextNode(child)) {
+      return <React.Fragment>{child.text}</React.Fragment>;
+    }
 
-  const renderText = (paragraph: SerializedParagraphNode, typoClassName: string) => {
-    return paragraph.children.map((child) => {
-      if ($isSerializedMentionNode<MentionData>(child)) {
-        return (
-          <Button
-            data-qa-anchor={`${pageId}/${componentId}/mention`}
-            key={child.data.userId}
-            className={clsx(typoClassName, styles.textWithMention__mention)}
-            onPress={() => goToUserProfilePage(child.data.userId)}
-          >
-            {child.text}
-          </Button>
-        );
-      }
-      if ($isSerializedAutoLinkNode(child) || $isSerializedLinkNode(child)) {
-        return (
-          <a
-            key={child.url}
-            href={child.url}
-            className={clsx(typoClassName, styles.textWithMention__link)}
-            data-qa-anchor={`${pageId}/${componentId}/post_link`}
-          >
-            {$isSerializedTextNode(child.children[0]) ? child.children[0]?.text : child.url}
-          </a>
-        );
-      }
+    return null;
+  };
 
-      if ($isSerializedTextNode(child)) {
-        return (
-          <span className={clsx(typoClassName, styles.textWithMention__text)}>{child.text}</span>
-        );
-      }
-
-      return null;
-    });
+  const renderText = (paragraph: SerializedParagraphNode[]) => {
+    return paragraph.map((p, index) => (
+      <React.Fragment key={index}>
+        {p.children.map((child, childIndex) => convertSerializedToText(child))}
+        <br />
+      </React.Fragment>
+    ));
   };
 
   return (
-    <div className={styles.textWithMention__container}>
-      <div
-        ref={fullContentRef}
-        className={styles.textWithMention__fullContent}
-        data-hidden={isHidden}
-      >
-        {editorState.root.children.map((paragraph, index) => {
-          return (
-            <p key={index} className={styles.textWithMention__paragraph}>
-              {paragraph.children.length > 0 ? (
-                <Typography.Body
-                  renderer={({ typoClassName }) => {
-                    return <>{renderText(paragraph, typoClassName)}</>;
-                  }}
-                />
-              ) : (
-                <br />
-              )}
-            </p>
-          );
-        })}
-      </div>
-      <div
-        data-qa-anchor={`${pageId}/${componentId}/text_with_mention`}
-        data-expanded={isExpanded}
-        className={styles.textWithMention__clamp}
-        style={
-          {
-            '--asc-text-with-mention-max-lines': maxLines,
-          } as React.CSSProperties
-        }
-      >
-        {isClamped && !isExpanded && (
-          <div onClick={() => setIsExpanded(true)} className={styles.textWithMention__showMoreLess}>
-            <Typography.Body>...See more</Typography.Body>
-          </div>
-        )}
-        {editorState.root.children.map((paragraph, index) => {
-          return (
-            <p className={styles.textWithMention__paragraph} key={index}>
-              {paragraph.children.length > 0 ? (
-                <Typography.Body
-                  renderer={({ typoClassName }) => {
-                    return <>{renderText(paragraph, typoClassName)}</>;
-                  }}
-                />
-              ) : (
-                <br />
-              )}
-            </p>
-          );
-        })}
-      </div>
-    </div>
+    <Typography.Body
+      renderer={({ typoClassName }) => (
+        <div className={clsx(styles.textWithMention__container, typoClassName)}>
+          {isExpanded ? (
+            renderText(editorState.root.children)
+          ) : (
+            <Truncate
+              lines={maxLines}
+              ellipsis={
+                <>
+                  ...
+                  <Button
+                    className={styles.textWithMention__seeMore}
+                    onPress={() => setIsExpanded(true)}
+                  >
+                    See more
+                  </Button>
+                </>
+              }
+            >
+              <div>{renderText(editorState.root.children)}</div>
+            </Truncate>
+          )}
+        </div>
+      )}
+    />
   );
 };
