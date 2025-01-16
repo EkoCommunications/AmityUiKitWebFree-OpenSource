a6417119b238eff4cf239687b4829170e8165cff
ChayanitBm <chayanit@amity.co>
2025-01-16 10:07:33 +0700

    fix: ASC-27381 - block user link (#854)
    * fix: block user link
    
    * refactor: remove div

--- src/v4/social/pages/BlockedUserPage/BlockedUserItem/BlockedUserItem.tsx
+++ src/v4/social/pages/BlockedUserPage/BlockedUserItem/BlockedUserItem.tsx
@@ -4,6 +4,8 @@ import { UserListUnblockUserButton } from '~/v4/social/elements/UserListUnblockU
 import { UserAvatar } from '~/v4/social/internal-components/UserAvatar';
 import { Typography } from '~/v4/core/components';
 import useUserBlock from '~/v4/social/hooks/useUserBlock';
+import { Button } from '~/v4/core/components/AriaButton';
+import { useNavigation } from '~/v4/core/providers/NavigationProvider';
 
 type BlockedUserItemProps = {
   pageId?: string;
@@ -17,12 +19,20 @@ export const BlockedUserItem: FC<BlockedUserItemProps> = ({
   componentId = '*',
 }) => {
   const { unblockUser } = useUserBlock();
+  const { goToUserProfilePage } = useNavigation();
   return (
     <div className={styles.blockUserItem}>
-      <UserAvatar userId={user.userId} className={styles.blockUserItem__avatar} />
-      <Typography.BodyBold className={styles.blockUserItem__displayName}>
-        {user.displayName}
-      </Typography.BodyBold>
+      <Button
+        className={styles.blockUserItem__user}
+        variant="text"
+        onPress={() => goToUserProfilePage(user.userId)}
+      >
+        <UserAvatar userId={user.userId} className={styles.blockUserItem__avatar} />{' '}
+        <Typography.BodyBold className={styles.blockUserItem__displayName}>
+          {user.displayName}
+        </Typography.BodyBold>
+      </Button>
+
       <UserListUnblockUserButton
         pageId={pageId}
         componentId={componentId}
