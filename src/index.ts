c827febe4fefd4ee8c019c0e5e88c2cdc7a94c46
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-01-17 12:33:35 +0700

    fix: ASC-27631 - post content padding (#869)
    * fix: post content padding top
    
    * fix: v4 social export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as AmityUiKitProvider } from '~/v4/core/providers/AmityUIKitProvider';
 export { default as AmityUiKitFeed } from '~/social/components/Feed';
-export { default as AmityUiKitSocial } from '~/social/pages/Application';
+export { default as AmityUiKitSocial } from '~/v4/social/pages/Application';
 export { default as AmityUiKitChat } from '~/chat/pages/Application';
 
 // Export helper
