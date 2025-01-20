99837914f450be6990ca95d1549e0ca8d6e6e59c
ChayanitBm <chayanit@amity.co>
2025-01-20 14:08:34 +0700

    fix: ASC-27544 - story show discard link modal  (#877)
    * fix: check empty link
    
    * fix: remove bottom sheet use drawer

--- src/v4/social/elements/DoneButton/DoneButton.tsx
+++ src/v4/social/elements/DoneButton/DoneButton.tsx
@@ -4,6 +4,7 @@ import { useAmityElement } from '~/v4/core/hooks/uikit';
 import styles from './DoneButton.module.css';
 import clsx from 'clsx';
 import { Button } from '~/v4/core/components/AriaButton';
+import { useResponsive } from '~/v4/core/hooks/useResponsive';
 
 export interface DoneButtonProps extends React.ComponentProps<typeof Button> {
   pageId?: string;
@@ -26,12 +27,13 @@ export function DoneButton({
     componentId,
     elementId,
   });
+  const { isDesktop } = useResponsive();
 
   if (isExcluded) return null;
 
   return (
     <Button
-      variant="fill"
+      variant={isDesktop ? 'fill' : 'text'}
       className={clsx(styles.doneButton, className)}
       data-qa-anchor={accessibilityId}
       isDisabled={isDisabled}
